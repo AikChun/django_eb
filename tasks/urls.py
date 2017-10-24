34ecb2a1db2968ba1965ce7b8597653c79b71523
aikchun <aikchun616@gmail.com>
2017-10-24 14:13:35 +0800

    Added a blank to upload to S3 Added button click to upload the file to S3 with timestamp

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 import tasks.views as views
 
 urlpatterns = [
-    url(r'^dashboard/$', views.dashboard, name="dashboard")
+    url(r'^dashboard/$', views.dashboard, name="dashboard"),
+    url(r'^upload-file/$', views.upload_file, name="upload-file.html")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from todo_app.settings import BASE_DIR
+
+import boto3
+import time
 
 
 # Create your views here.
 def dashboard(request):
     return render(request, "dashboard.html")
+
+
+def upload_file(request):
+
+    # Create an S3 client
+    s3 = boto3.client('s3')
+
+    filename = "file.txt"
+    path_to_file = '{0}/{1}'.format(BASE_DIR, filename)
+    bucket_name = 'last-hearth'
+
+    # Uploads the given file using a managed uploader, which will split up large
+    # files automatically and upload parts in parallel.
+    s3.upload_file(path_to_file, bucket_name, "{0}.{1}".format(filename, str(time.time())))
+    return render(request, "upload-file.html")
